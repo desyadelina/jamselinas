a0e2ea958a12bd85216c05009ca792906559413e
O12097 <desyaderina@gmail.com>
2025-06-12 11:08:59 +0800

    ref: adjust gallery image container for consistent aspect ratio

--- resources/views/pages/landing/_gallery.blade.php
+++ resources/views/pages/landing/_gallery.blade.php
@@ -4,12 +4,9 @@
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         @for ($i = 1; $i <= 8; $i++)
-            <div class="h-52 rounded-xl shadow overflow-hidden flex items-center justify-center bg-gray-100">
-                <img 
-                    src="{{ asset('images/gallery/gallery_' . $i . '.png') }}" 
-                    alt="Gallery {{ $i }}" 
-                    class="object-cover w-full h-full"
-                >
+            <div class="rounded-xl shadow overflow-hidden flex items-center justify-center bg-gray-100 aspect-[326/219]">
+                <img src="{{ asset('images/gallery/gallery_' . $i . '.png') }}" alt="Gallery {{ $i }}"
+                    class="object-cover w-full h-full">
             </div>
         @endfor
     </div>
