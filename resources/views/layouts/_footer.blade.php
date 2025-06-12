940a2b8d0659e40b7afd43bf53ec7ce74256dc7e
O12097 <desyaderina@gmail.com>
2025-06-12 11:45:33 +0800

    ref: update footer layout and content for JAMSELINAS XIV 2025

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,45 +1,59 @@
 <footer class="w-full bg-jamselinas-footer py-8">
-    <div class="container mx-auto grid grid-cols-1 lg:grid-cols-4 gap-8">
-        <div class="flex flex-col items-center lg:items-start">
-            <img src="{{ asset('images/large_nobg_logo.png') }}" alt="JAMSELINAS Logo"
-                class="w-[308.17px] object-contain rounded-lg mb-4">
-            <p class="text-black text-center lg:text-left max-w-xs">
-                <span class="font-normal text-[16px]">Banjarmasin Tuan Rumah </span>
-                <span class="font-bold text-[18px]">JAMSELINAS XIV 2025</span>
-                <span class="font-normal text-[16px]">: Festival Sepeda Lipat Terbesar di Indonesia</span>
-            </p>
-        </div>
-        <div class="flex flex-col justify-start h-full">
-            <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Useful Links</h3>
-            <div class="flex flex-col gap-2 text-[22px]">
-                <ul class="list-disc list-inside text-black">
-                    <li>
-                        <a href="#" class="hover:text-jamselinas-primary">Home</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:text-jamselinas-primary">Event Guide</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:text-jamselinas-primary">News</a>
-                    </li>
-                </ul>
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-1 lg:grid-cols-[420px_1fr_1fr_1fr] gap-8 mb-8">
+            <div class="flex flex-col items-center lg:items-start">
+                <img src="{{ asset('images/large_nobg_logo.png') }}" alt="JAMSELINAS Logo"
+                    class="w-[308.17px] object-contain rounded-lg mb-4 mx-auto block">
+                <p class="text-black text-center lg:text-left">
+                    <span class="font-normal text-[16px]">Banjarmasin Tuan Rumah </span><span
+                        class="font-bold text-[18px]">JAMSELINAS XIV 2025</span><span
+                        class="font-normal text-[16px]">:</span>
+                    <br>
+                    <span class="font-normal text-[16px]">Festival Sepeda Lipat Terbesar di Indonesia</span>
+                </p>
+            </div>
+            <div class="flex flex-col justify-start h-full">
+                <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Useful Links</h3>
+                <div class="flex flex-col gap-2 text-[22px]">
+                    <ul class="list-disc list-inside text-black">
+                        <li>
+                            <a href="#" class="hover:text-jamselinas-primary">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-jamselinas-primary">Event Guide</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-jamselinas-primary">News</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-start h-full">
+                <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Event Features</h3>
+                <div class="flex flex-col gap-2 text-[22px]">
+                    <ul class="list-disc list-inside text-black">
+                        <li><a href="#" class="text-black hover:text-jamselinas-primary">Route Map</a></li>
+                        <li><a href="#" class="text-black hover:text-jamselinas-primary">Merchandise</a></li>
+                        <li><a href="#" class="text-black hover:text-jamselinas-primary">Photo Gallery</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="flex flex-col justify-start h-full">
+                <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Information</h3>
+                <div class="flex flex-col gap-2 text-[22px]">
+                    <a href="tel:08888888888" class="text-black hover:text-jamselinas-primary">08888888888</a>
+                    <a href="tel:082155555555" class="text-black hover:text-jamselinas-primary">082155555555</a>
+                </div>
             </div>
         </div>
-        <div class="flex flex-col justify-start h-full">
-            <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Event Features</h3>
-            <div class="flex flex-col gap-2 text-[22px]">
-                <ul class="list-disc list-inside text-black">
-                    <li><a href="#" class="text-black hover:text-jamselinas-primary">Route Map</a></li>
-                    <li><a href="#" class="text-black hover:text-jamselinas-primary">Merchandise</a></li>
-                    <li><a href="#" class="text-black hover:text-jamselinas-primary">Photo Gallery</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="flex flex-col justify-start h-full">
-            <h3 class="font-bold text-[28px] text-black mb-4 mt-8">Information</h3>
-            <div class="flex flex-col gap-2 text-[22px]">
-                <a href="tel:08888888888" class="text-black hover:text-jamselinas-primary">08888888888</a>
-                <a href="tel:082155555555" class="text-black hover:text-jamselinas-primary">082155555555</a>
+
+        <div class="border-t border-black/20 pt-6">
+            <div class="text-center">
+                <p class="text-black text-[16px]">
+                    © 2025 JAMSELINAS XIV. All rights reserved.
+                </p>
             </div>
         </div>
     </div>
